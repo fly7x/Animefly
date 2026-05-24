aec6ffc35c163862e9268c9f6724fc6968182c21
fly7x <moode6019@gmail.com>
2026-05-24 20:21:11 +0400

    Update page.js

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 import SearchClient from "@/components/SearchClient";
 
 export const viewport = { themeColor: "#07060b" };
-export const metadata = { title: "Search Anime — AnimeDex" };
+export const metadata = { title: "Search Anime — Fly Anime" };
 
 export default function SearchPage() {
   return <SearchClient />;
