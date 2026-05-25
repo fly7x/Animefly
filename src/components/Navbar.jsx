5cdb845c877879315d742b9e99768c8e1038a674
fly7x <moode6019@gmail.com>
2026-05-25 17:42:18 +0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ export default function Navbar() {
             whileHover={{ scale: 1.08 }}
             whileTap={{ scale: 0.95 }}
           >
-            <AnimeDexIcon />
+            <FlyAnimeIcon />
           </motion.div>
           <span className={styles.logoText}>
             <span className={styles.logoMain}>Anime</span><span className={styles.logoDex}>Dex</span>
