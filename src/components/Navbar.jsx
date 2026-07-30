73e7e58f85e9248888408bacf4049bab64e034a9
fly7x <moode6019@gmail.com>
2026-07-31 02:47:19 +0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -186,11 +186,6 @@ export default function Navbar() {
     Login
   </motion.a>
 )}
-<button style={settingsIconStyle} onClick={() => setShowSettings(true)} title="Settings">
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <circle cx="12" cy="12" r="3"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14M4.93 4.93a10 10 0 0 0 0 14.14"/>
-  </svg>
-</button>
 
 <SettingsPanel open={showSettings} onClose={() => setShowSettings(false)} />
 
